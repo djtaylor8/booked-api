c106a228636e17f85586b7c76fb4c720a389e472
DJ <dtaylor88@gmail.com>
2021-11-24 12:52:41 -0800

    set up basics for first audition query

--- src/index.js
+++ src/index.js
@@ -4,13 +4,35 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    auditions: [Audition!]!
+  }
+
+  type Mutation {
+    post(location: String!, description: String!): Audition!
+  }
+
+  type Audition {
+      id: ID!
+      description: String!
+      location: String!
   }
 `
+let auditions = [{
+    id: 'audition-0',
+    location: 'Sample Casting Agency',
+    description: 'Frightened Inmate #2'
+  }]
 
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the Booked API test`
+    info: () => `This is the Booked API test`,
+    auditions: () => auditions,
+  },
+  Audition: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      location: (parent) => parent.location,
   }
 }
 
